ea96940c1c5f9cd285d8be93b1e25445c060610c
Shanukathushara <130734269+Shanukathushara@users.noreply.github.com>
2024-05-25 13:03:15 +0530

    Add files via upload
    These are the functions used in C language.

---
+++ Functions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void birthday()
+{
+    printf("\nHappy birthday to you!");
+    printf("\nHave a good one!");
+}
+
+int main()
+{
+    birthday();
+    birthday();
+    birthday();
+
+    return 0;
+}
